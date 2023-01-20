a6a9c08e29e0b9a277e65b231aaab67310c64288
James <james31982@gmail.com>
2023-01-20 12:33:44 -0600

    github PARAMS ADDED

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ import os
 import yaml
 import joblib
 import numpy as np
+from prediction_service import prediction
 
-params_path = "params.yaml"
+#params_path = "params.yaml" (removed for prediction_service)
 webapp_root = "webapp"
 
 static_dir = os.path.join(webapp_root, "static")
@@ -12,49 +13,56 @@ template_dir = os.path.join(webapp_root, "templates")
 
 app = Flask(__name__, static_folder = static_dir, template_folder = template_dir)
 
-# Create prediction function
+# Create prediction function (blocked commented for prediciton_service)
 
-def read_params(config_path):
-    with open(config_path) as yaml_file:
-        config = yaml.safe_load(yaml_file)
-    return config
+#def read_params(config_path):
+#    with open(config_path) as yaml_file:
+#        config = yaml.safe_load(yaml_file)
+#    return config
 
-def predict(data):
-    config = read_params(params_path)
-    model_dir_path = config["webapp_model_dir"]
-    model = joblib.load(model_dir_path)
-    prediction = model.predict(data)
-    print(prediction)
-    return prediction[0]
+#def predict(data):
+#    config = read_params(params_path)
+#    model_dir_path = config["webapp_model_dir"]
+#    model = joblib.load(model_dir_path)
+#    prediction = model.predict(data)
+#    print(prediction)
+#    return prediction[0]
 
-def api_response(request):
-    try:
-        data = np.array([list(request.json.values())])
-        response = predict(data)
-        response = {"response": response}
-        return response
-    except Exception as e:
-        print(e)
-        error = {"error": "Something went wrong!!! Try again"}
-        return error
+#def api_response(request):
+#    try:
+#        data = np.array([list(request.json.values())])
+#        response = predict(data)
+#        response = {"response": response}
+#        return response
+#    except Exception as e:
+#        print(e)
+#        error = {"error": "Something went wrong!!! Try again"}
+#        return error
 
 @app.route("/", methods = ["GET", "POST"])
 def index():
     if request.method == "POST":
         try:
             if request.form:
-                data = dict(request.form).values()
-                data = [list(map(float, data))]
-                response = predict(data)
+                # new lines for predicition_service
+                data_req = dict(request.form)
+                response = prediction.form_response(data_req)
+
+                #data = dict(request.form).values()  commented for prediction_service
+                #data = [list(map(float, data))]     commented for prediction_Service
+                #response = predict(data)            commented for prediction_service
                 return render_template("index.html", response = response)
             elif request.json:
-                response = api_response(request)
+                #response = api_response(request) commented for prediction_service
+                
+                # new line
+                response = prediction.api_response(request.json)
                 return jsonify(response)
 
         except Exception as e:
             print(e)
-            error = {"error": "Something went wrong!!! Try again"}
-            return render_template("404.html", error = error)
+            #error = {"error": "Something went wrong!!! Try again"}
+            return render_template("404.html", error = e)  # previously was error = error
 
     else:
         return render_template("index.html")
